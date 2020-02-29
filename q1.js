67735cb59d6ed94e4bad57216963209cb9d3bb70
anmolttn <60728941+anmolttn@users.noreply.github.com>
2020-02-29 21:20:06 +0530

    Delete q1.js

--- q1.js
+++
@@ -1,7 +0,0 @@
-var amount = prompt("Enter the amount", "");
-var interest = prompt("Enter the rate of interest", "");
-var years = prompt("Enter the number of years", "")
-var a = parseInt(amount);
-var i = parseInt(interest);
-var r = parseInt(years);
-var si = ((a*i*r)/100);
