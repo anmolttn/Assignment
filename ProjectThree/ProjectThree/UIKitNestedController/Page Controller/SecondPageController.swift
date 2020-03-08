27cc620169d77e4debdfc1773b1c833bb4d484f4
anmolttn <anmol.chauhan@tothenew.com>
2020-03-09 01:09:27 +0530

    Assignment Update

---
+++ ProjectThree/ProjectThree/UIKitNestedController/Page Controller/SecondPageController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondPageController.swift
+//  ProjectThree
+//
+//  Created by Anmol Chauhan on 08/03/20.
+//  Copyright © 2020 Anmol Chauhan. All rights reserved.
+//
+
+import UIKit
+
+class SecondPageController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //print("Second")
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ProjectThree/ProjectThree/UIKitNestedController/TabBarProgramatically.swift
+++ ProjectThree/ProjectThree/UIKitNestedController/TabBarProgramatically.swift
@@ -13,18 +13,22 @@ class TabBarProgramatically2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.title = "Tab Bar Programmatically"
         createTabBarProgrammatically()
     }
     
+    //Task 1: Create Tab Bar Programmatically and from storyboard.
+
+    //create a tab bar programmaticaaly
     func createTabBarProgrammatically(){
             self.view.backgroundColor = .brown
             let bar = UITabBar(frame: CGRect(x: 0, y: self.view.frame.height-150, width: self.view.frame.width, height: 100))
             self.view.addSubview(bar)
 
-            let itemOne = UITabBarItem(title: "One", image: nil, selectedImage: nil)
-            let itemTwo = UITabBarItem(title: "Two", image: nil, selectedImage: nil)
-            let itemThree = UITabBarItem(title: "Three", image: nil, selectedImage: nil)
-            let itemFour = UITabBarItem(title: "Four", image: nil, selectedImage: nil)
+            let itemOne = UITabBarItem(title: "First", image: nil, selectedImage: nil)
+            let itemTwo = UITabBarItem(title: "Second", image: nil, selectedImage: nil)
+            let itemThree = UITabBarItem(title: "Third", image: nil, selectedImage: nil)
+            let itemFour = UITabBarItem(title: "Fourth", image: nil, selectedImage: nil)
            
             bar.setItems([itemOne,itemTwo,itemThree,itemFour], animated: true)
             bar.backgroundColor = .black
